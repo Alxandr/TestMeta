d4f6c64b15a535a7e03253cceb19f86fa0b6b0e5
Alxandr <alxandr@alxandr.me>
2014-06-29 20:29:01 +0200

    Init

---
+++ Meta/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Framework.Runtime;
+
+namespace Meta
+{
+    public class Processor : IMetaProcessor
+    {
+        public Processor() { }
+
+        public CSharpCompilation Process(CSharpCompilation compilation)
+        {
+            Console.WriteLine("Did it works?");
+            var trees = compilation.SyntaxTrees;
+            foreach(var st in trees)
+            {
+                var rewriter = new RewriteHelloWorld();
+                var newRoot = rewriter.Visit(st.GetRoot());
+                compilation = compilation.ReplaceSyntaxTree(st, st.WithRootAndOptions(newRoot, st.Options));
+            }
+
+            return compilation;
+        }
+    }
+
+    public class RewriteHelloWorld : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitParameter(ParameterSyntax node)
+        {
+            return base.VisitParameter(node);
+        }
+    }
+}
---
+++ Meta/Services/IMetaProcessor.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Microsoft.Framework.Runtime
+{
+    [AssemblyNeutral]
+    public interface IMetaProcessor
+    {
+        CSharpCompilation Process(CSharpCompilation compilation);
+    }
+}
